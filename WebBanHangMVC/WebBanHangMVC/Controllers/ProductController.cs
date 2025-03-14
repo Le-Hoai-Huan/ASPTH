5db87c76a38bcb8d07cf12d692e07a336e967030
Le-Hoai-Huan <149983414+Le-Hoai-Huan@users.noreply.github.com>
2025-03-14 13:34:56 +0700

    lab03

---
+++ WebBanHangMVC/WebBanHangMVC/Controllers/ProductController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebBanHangMVC.Models;
+using WebBanHangMVC.Repository;
+
+namespace WebBanHangMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+
+        public ProductController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllAsync();
+            return View(products);
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product product, IFormFile imageUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageUrl != null)
+                {
+                    product.ImageUrl = await SaveImage(imageUrl);
+                }
+                await _productRepository.AddAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+            return View(product);
+        }
+        private async Task<string> SaveImage(IFormFile image)
+        {
+            var savePath = Path.Combine("wwwroot/images", image.FileName);
+            using (var fileStream = new FileStream(savePath, FileMode.Create))
+            {
+                await image.CopyToAsync(fileStream);
+            }
+            return "/images/" + image.FileName; // Trả về đường dẫn tương đối
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+            return View(product);
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id ,Product product,IFormFile imageUrl)
+        {
+            ModelState.Remove("ImagesUrl");
+            if(id!= product.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                var products = await _productRepository.GetByIdAsync(id);
+                if(imageUrl == null)
+                {
+                    product.ImageUrl = products.ImageUrl;
+                }
+                else
+                {
+                    product.ImageUrl = await SaveImage(imageUrl);
+                }
+
+                products.Price = product.Price;
+                products.Description = product.Description;
+                products.CategoryId = product.CategoryId;
+                products.ImageUrl = product.ImageUrl;
+
+                await _productRepository.UpadteAsync(products);
+
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = await _categoryRepository.GetAllAsync();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+            return View(product);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        
+        [HttpPost, ActionName("DeleteConfirmed")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _productRepository.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
